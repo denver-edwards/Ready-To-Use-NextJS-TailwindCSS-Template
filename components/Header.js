ef5bf638c2a98c1a7f4da501d896a6918a39d680
Unsignia <csdedwards@gmail.com>
2022-04-30 17:14:50 -0400

    added scrollbar

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { useState } from "react";
 import { useTheme } from "next-themes";
+import { Squash as Hamburger } from "hamburger-react";
 
 export default function Header() {
   const { theme, setTheme } = useTheme();
@@ -47,41 +48,24 @@ export default function Header() {
               ></path>
             </svg>
           </button>
+
           <button
             type="button"
             className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
             onClick={() => setShowMenu(!showMenu)}
           >
             <span className="sr-only">Open main menu</span>
-            <svg
-              className="w-6 h-6"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
-            <svg
-              className="hidden w-6 h-6"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
+            <Hamburger size={20} toggled={showMenu} toggle={setShowMenu} />
           </button>
         </div>
 
         <div
-          className={"w-full md:block md:w-auto" + (showMenu ? " hidden" : "")}
+          className={
+            "w-full md:block md:w-auto" +
+            (!showMenu
+              ? " hidden"
+              : " transition-transform duration-500  ease-linear")
+          }
           id="mobile-menu"
         >
           <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
